8f21aeea63c1c418609bdacfc46e160eb47c6e76
SakuHeli0s <mariabayanescobar@gmail.com>
2026-03-28 00:18:28 +0100

    Retoques finales.

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ const menuNav = document.querySelector('.menu-nav')
 menuHamburguer.addEventListener('click', () =>{
     menuNav.classList.toggle('active')
 })
+
+//FLECHA QUE DESAPARECE
+
+const arrowUp = document.querySelector('.footer-up')
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > window.innerHeight / 2){
+        arrowUp.classList.add('visible')
+    }else{
+        arrowUp.classList.remove('visible')
+    }
+})
